482f06b9e6a20d9bfedc5c1dd6b2a55873f92047
Kritik Ranjit <kritik.ranjit4@gmail.com>
2019-12-11 23:02:00 +0545

    Bottom Navigation with Home, Notification and Settings fragments

--- app/src/main/java/com/example/myhealthcare/Dashboard.java
+++ app/src/main/java/com/example/myhealthcare/Dashboard.java
@@ -1,21 +1,65 @@
 package com.example.myhealthcare;
 
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-public class Dashboard extends AppCompatActivity{
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Dashboard extends AppCompatActivity {
+
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+
+    private HomeFragment homeFragment;
+    private NotificationFragment notificationFragment;
+    private SettingsFragment settingsFragment;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        mMainNav = (BottomNavigationView) findViewById(R.id.main_nav);
+
+        homeFragment = new HomeFragment();
+        notificationFragment = new NotificationFragment();
+        settingsFragment = new SettingsFragment();
+
+        setFragment(homeFragment);
+
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        setFragment(homeFragment);
+                        return true;
+
+                    case R.id.nav_notification:
+                        setFragment(notificationFragment);
+                        return true;
+
+                    case R.id.nav_settings:
+                        setFragment(settingsFragment);
+                        return true;
+
+                    default:
+                        return false;
+                }
+            }
+        });
     }
 
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
+    }
 }
