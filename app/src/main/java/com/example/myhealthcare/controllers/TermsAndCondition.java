e11b11e5633ae99190093a0c0a922bee85afd260
Kritik Ranjit <kritik.ranjit4@gmail.com>
2020-01-27 21:53:58 +0545

    some updates

---
+++ app/src/main/java/com/example/myhealthcare/controllers/TermsAndCondition.java
@@ -0,0 +1,16 @@
+package com.example.myhealthcare.controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myhealthcare.R;
+
+public class TermsAndCondition extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_and_condition);
+    }
+}
