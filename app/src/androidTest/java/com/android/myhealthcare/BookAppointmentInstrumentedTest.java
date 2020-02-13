10891cccb275d8be51682b353e1a19f560b3d150
Kritik Ranjit <kritik.ranjit4@gmail.com>
2020-02-13 21:20:40 +0545

    Book Appointment Instrumented Test

--- app/src/androidTest/java/com/android/myhealthcare/BookAppointmentInstrumentedTest.java
+++ app/src/androidTest/java/com/android/myhealthcare/BookAppointmentInstrumentedTest.java
@@ -6,6 +6,7 @@ import androidx.test.rule.ActivityTestRule;
 
 import com.android.myhealthcare.controllers.Doctor;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,28 +14,31 @@ import org.junit.runner.RunWith;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
-public class DoctorInstrumentedTest {
+public class BookAppointmentInstrumentedTest {
 
     @Rule
     public ActivityTestRule<Doctor> testRule = new ActivityTestRule<>(Doctor.class);
 
+    @Before
+    public void setupHomeFragment() {
+        testRule.getActivity().getSupportFragmentManager().beginTransaction();
+    }
+
     @Test
-    public void testDoctor() {
+    public void testBookAppointment() {
         // verify that the doctor recycler view is displayed
         onView(withId(R.id.rv_doctors)).check(matches(isDisplayed()));
         // click the first item in the recycler view
         onView(withId(R.id.rv_doctors))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 
-        onView(withText("Location"))
-                .inRoot(withDecorView(not(testRule.getActivity().getWindow().getDecorView()))).check(matches(isDisplayed()));
+        onView(withId(R.id.bookappointment)).perform(click());
+
+        onView(withId(R.id.btnbookapp)).perform(click());
 
     }
 }
